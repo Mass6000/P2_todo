5444747d766202d84940ffc1bfa53614041376b9
John Chase <john.chase@live.com>
2019-02-27 16:49:06 -0700

    Exploring adding list names. May need to give up on content editable because of issues with return key etc.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-$('.listRows').on('click', function () {
-    document.execCommand('selectAll', false, null);
-});
+let taskMain = [],
+    listMain = [];
 
-let taskMain = [];
-let listMain = [];
+
+// Global Variables
+listCount = 0;
+
+class List {
+    constructor(listName) {
+        this.listName = listName;
+        this.listChosen = false;
+    }
+
+    changeListName(listName) {
+        this.listName = listName;
+    }
+
+    listPicked() {
+        this.listChosen = true;
+    }
+
+    listUnPicked() {
+        this.listChosen = false;
+    }
+}
+
 
 class Task {
     constructor(listName, taskName) {
@@ -15,41 +35,52 @@ class Task {
     changeTaskName(taskName) {
         this.taskName = taskName;
     }
+
     completeTask() {
         this.taskComplete = true;
     }
+
     unCompleteTask() {
         this.taskComplete = false;
     }
 }
 
-class List {
-    constructor(listName) {
-        this.listName = listName;
-        this.listChosen = false;
-    }
-    changeListName(listName) {
-        this.listName = listName;
-    }
-    listPicked() {
-        this.listChosen = true;
-    }
-    listUnPicked() {
-        this.listChosen = false;
+// This is where input come into the program
+
+
+$('.listRow').on('click', function () {
+    document.execCommand('selectAll', false, null);
+});
+
+function addList(myList, event) {
+    switch (event.key) {
+        case 'Enter':
+            listCount ++;
+            $('#theLists').append('<div id="listName' + listCount
+                + '"><i id="list-fa-circle' + listCount
+                + '" class="fas fa-circle"></i><i id="list-fa-check-circle' + listCount
+                + '" class="fas fa-check-circle"></i><span class="listRow" contenteditable="true" onkeyup="addList(this.value, event">'
+                + myList + '</span><i id="list-fa-minus-circle' + listCount
+                + '" class="fas fa-minus-circle"></i></div>');
+            break;
+
+
     }
 }
 
 
-
-task = new Task('My List', 'One');
-taskMain.push(task);
-console.log(taskMain);
-task = new Task('My List', 'Two');
-taskMain.push(task);
+$('#list-fa-check-circle0').hide();
 
 
-taskMain[0].changeTaskName('1');
-taskMain[1].completeTask();
-taskMain[1].unCompleteTask();
-taskMain[0].changeTaskName('One');
-console.log(taskMain);
+// task = new Task('My List', 'One');
+// taskMain.push(task);
+// console.log(taskMain);
+// task = new Task('My List', 'Two');
+// taskMain.push(task);
+
+
+// taskMain[0].changeTaskName('1');
+// taskMain[1].completeTask();
+// taskMain[1].unCompleteTask();
+// taskMain[0].changeTaskName('One');
+// console.log(taskMain);
